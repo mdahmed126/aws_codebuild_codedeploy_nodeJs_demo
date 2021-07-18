edc476cee2324c04640eaab06b2359e6312c3025
Md Ahmed <82947351+mdahmed126@users.noreply.github.com>
2021-07-18 15:16:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V1000!')
+  res.send('CICD App V2000!')
 })
 
 app.get('/status', (req, res) => {
